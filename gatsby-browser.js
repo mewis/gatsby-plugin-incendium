d6f789a836c8fc6ffb68784334f3ca45f73abad1
Lewis Bayfield <bassymew@gmail.com>
2020-12-08 10:06:24 +0000

    Run only only second + page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,13 @@
 
-export const onRouteUpdate = () => {
-  if (typeof window.inc == "function") {
-    window.inc("run")
+export const onRouteUpdate = ({prevLocation}) => {
+  // there does not seem to be a way in gatsby to run "before page change"
+  if (prevLocation) {
+    if (typeof window.inc == "function") {
+      window.inc("send_pageview_meta")
+    }
+
+    if (typeof window.inc == "function") {
+      window.inc("run")
+    }
   }
 }
