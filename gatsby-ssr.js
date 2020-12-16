5f170e8bb32b18d38dababf17441b4781dbb1d0f
Lewis Bayfield <bassymew@gmail.com>
2020-12-16 17:44:44 +0000

    Fix plugin syntax error

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,7 @@ export const onRenderBody = (
         __html: [
           'var head = document.head || document.getElementsByTagName("head")[0];',
           'var script = document.createElement("script");',
-          `script.setAttribute("src", "${pluginOptions.url}${pluginOptions.nonInteractive ? '?non_interactive' : ''}")`,
+          `script.setAttribute("src", "${pluginOptions.url}${pluginOptions.nonInteractive ? '?non_interactive' : ''}");`,
           'head.appendChild(script);',
         ].join(process.env.NODE_ENV === 'production' ? '' : '\n'),
       }}
