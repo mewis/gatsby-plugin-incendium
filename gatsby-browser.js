22af35c5eb3359d18a75c3164dfd81233bd2bc12
Lewis Bayfield <bassymew@gmail.com>
2022-11-18 11:15:15 +0000

    Impreove on route change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,12 +8,27 @@ var onRouteUpdate = function onRouteUpdate(_ref) {
 
   // there does not seem to be a way in gatsby to run "before page change"
   if (prevLocation) {
-    if (typeof window.inc == "function") {
-      window.inc("send_pageview_meta");
-    }
+    var t = 16;
 
     if (typeof window.inc == "function") {
-      window.inc("run");
+      if ("requestAnimationFrame" in window) {
+        requestAnimationFrame(function () {
+          requestAnimationFrame(function () {
+            window.inc("send_pageview_meta");
+            setTimeout(function () {
+              return window.inc("run", true);
+            }, t * 2);
+          });
+        });
+      } else {
+        // Delay by 32ms to simulate 2 requestOnAnimationFrame calls
+        setTimeout(function () {
+          window.inc("send_pageview_meta");
+        }, t * 2);
+        setTimeout(function () {
+          window.inc("run", true);
+        }, t * 4);
+      }
     }
   }
 };
--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,13 +1,24 @@
-
-export const onRouteUpdate = ({prevLocation}) => {
+export const onRouteUpdate = ({ prevLocation }) => {
   // there does not seem to be a way in gatsby to run "before page change"
   if (prevLocation) {
+    const t = 16;
     if (typeof window.inc == "function") {
-      window.inc("send_pageview_meta")
-    }
-
-    if (typeof window.inc == "function") {
-      window.inc("run")
+      if (`requestAnimationFrame` in window) {
+        requestAnimationFrame(() => {
+          requestAnimationFrame(() => {
+            window.inc("send_pageview_meta");
+            setTimeout(() => window.inc("run", true), t * 2);
+          });
+        });
+      } else {
+        // Delay by 32ms to simulate 2 requestOnAnimationFrame calls
+        setTimeout(() => {
+          window.inc("send_pageview_meta");
+        }, t * 2);
+        setTimeout(() => {
+          window.inc("run", true);
+        }, t * 4);
+      }
     }
   }
-}
+};
