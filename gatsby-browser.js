35967d3c4695cf4a0d86892b09f889d36729655e
Lewis Bayfield <bassymew@gmail.com>
2024-02-08 12:52:45 +0000

    fix for referrer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,11 +3,15 @@
 exports.__esModule = true;
 exports.onRouteUpdate = void 0;
 
-var onRouteUpdate = function onRouteUpdate() {
+var onRouteUpdate = function onRouteUpdate(_ref) {
+  var prevLocation = _ref.prevLocation;
+
   var checkForScript = function checkForScript() {
     if (typeof window.inc !== "undefined") {
-      window.inc("send_pageview_meta");
-      window.inc("run", false);
+      window.inc("send_pageview_meta"); // second param is ignore referer which we do if there is a previous pageview
+      // this is becouse naviagting via gatsby, no page refresh happens so document.referrer persists
+
+      window.inc("run", !!prevLocation);
     } else {
       // Script not loaded yet, check again after a short delay
       setTimeout(checkForScript, 100);
--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,8 +1,10 @@
-export const onRouteUpdate = () => {
+export const onRouteUpdate = ({ prevLocation }) => {
   const checkForScript = () => {
     if (typeof window.inc !== "undefined") {
       window.inc("send_pageview_meta");
-      window.inc("run", false);
+      // second param is ignore referer which we do if there is a previous pageview
+      // this is becouse naviagting via gatsby, no page refresh happens so document.referrer persists
+      window.inc("run", !!prevLocation);
     } else {
       // Script not loaded yet, check again after a short delay
       setTimeout(checkForScript, 100);
